1f857dd4bab821616dcbcd10d16d0ce2bddc4c10
Ralph Cajipe <50406636+ralphcajipe@users.noreply.github.com>
2023-03-26 08:49:31 +0000

    discard python.py & fix GUI.py

--- PythonSourcewGUI.py
+++ PythonSourcewGUI.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 
+
 class CreditCardValidatorGUI:
     def __init__(self, master):
         self.master = master
@@ -31,30 +32,33 @@ class CreditCardValidatorGUI:
                 else:
                     self.result_label.config(text="INVALID", fg="red")
 
+
 def isValid(n, type):
     """Check the validity"""
-    if (type == "VISA" and (len(str(n)) == 13 or len(str(n)) == 16)):
+    if type == "VISA" and (len(str(n)) == 13 or len(str(n)) == 16):
         return True
-    elif (type == "AMEX" and len(str(n)) == 15):
+    elif type == "AMEX" and len(str(n)) == 15:
         return True
-    elif (type == "MASTERCARD" and len(str(n)) == 16):
+    elif type == "MASTERCARD" and len(str(n)) == 16:
         return True
     else:
         return False
 
+
 def getType(n):
     """Get what type of card"""
     # visa
-    if (int(str(n)[0]) == 4 and checksum(n)):
+    if int(str(n)[0]) == 4 and checksum(n):
         return "VISA"
     # amex
-    elif ((int(str(n)[:2]) == 34 or int(str(n)[:2]) == 37) and checksum(n)):
+    elif (int(str(n)[:2]) == 34 or int(str(n)[:2]) == 37) and checksum(n):
         return "AMEX"
     # mastercard
-    elif ((int(str(n)[:2]) >= 51 and int(str(n)[:2]) <= 55) and checksum(n)):
+    elif (int(str(n)[:2]) >= 51 and int(str(n)[:2]) <= 55) and checksum(n):
         return "MASTERCARD"
     else:
         return "INVALID"
+
 
 def checksum(n):
     """Luhn's algorithm"""
@@ -63,17 +67,18 @@ def checksum(n):
     sum = 0
     for i in range(len(str(n)) - 2, -1, -2):
         sum += int(str(n)[i]) * 2
-        if (int(str(n)[i]) * 2 > 9):
+        if int(str(n)[i]) * 2 > 9:
             sum += 1
     # add the sum to the sum of the digits that weren’t multiplied by 2.
     for i in range(len(str(n)) - 1, -1, -2):
         sum += int(str(n)[i])
     # if the total’s last digit is 0(or, put more formally, if the total modulo 10 is congruent to 0),
     # the number is valid!
-    if (sum % 10 == 0):
+    if sum % 10 == 0:
         return True
     else:
         return False
+
 
 if __name__ == "__main__":
     root = tk.Tk()
