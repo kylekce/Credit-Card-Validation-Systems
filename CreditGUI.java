1826331676f0d29d82bce24270de58dee450570c
Ralph Cajipe <50406636+ralphcajipe@users.noreply.github.com>
2023-03-27 13:13:35 +0800

    now with good comments

--- CreditGUI.java
+++ CreditGUI.java
@@ -7,18 +7,23 @@ import java.awt.event.ActionListener;
 
 public class CreditGUI {
     public static void main(String[] args) {
+        // Create and display the main GUI frame on the event dispatch thread
         SwingUtilities.invokeLater(() -> {
+            // Create the main GUI frame
             JFrame frame = new JFrame("Card Validator");
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             frame.setSize(500, 300);
 
+            // Create the main panel for the GUI
             JPanel mainPanel = new JPanel();
             mainPanel.setLayout(new BorderLayout(10, 10));
             mainPanel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
 
+            // Create the input panel for the card number input field
             JPanel inputPanel = new JPanel();
             inputPanel.setLayout(new GridLayout(0, 1));
 
+            // Create the card number input label and field
             JLabel inputLabel = new JLabel("Enter Card Number:");
             inputLabel.setFont(new Font("Arial", Font.BOLD, 14));
             JTextField inputField = new JTextField(20);
@@ -26,20 +31,22 @@ public class CreditGUI {
             inputPanel.add(inputLabel);
             inputPanel.add(inputField);
 
+            // Create the result text area and add it to a scroll pane
             JTextArea resultArea = new JTextArea();
             resultArea.setEditable(false);
             resultArea.setFont(new Font("Arial", Font.BOLD, 18));
             resultArea.setLineWrap(true);
             resultArea.setWrapStyleWord(true);
-
             JScrollPane resultScrollPane = new JScrollPane(resultArea);
             resultScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
 
+            // Create the check button and attach an action listener to it
             JButton checkButton = new JButton("Check");
             checkButton.setFont(new Font("Arial", Font.BOLD, 14));
             checkButton.addActionListener(new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent e) {
+                    // Validate the card number input and display the result
                     long n;
                     try {
                         n = Long.parseLong(inputField.getText().trim());
@@ -48,7 +55,6 @@ public class CreditGUI {
                         resultArea.setForeground(Color.RED);
                         return;
                     }
-
                     String type = getType(n);
                     if (type.equals("INVALID")) {
                         resultArea.setText("INVALID");
@@ -65,32 +71,37 @@ public class CreditGUI {
                 }
             });
 
+            // Create the reset button and attach an action listener to it
             JButton resetButton = new JButton("Reset");
             resetButton.setFont(new Font("Arial", Font.BOLD, 14));
             resetButton.addActionListener(new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent e) {
+                    // Reset the card number input and result text area
                     inputField.setText("");
                     resultArea.setText("");
                     resultArea.setForeground(Color.BLACK);
                 }
             });
 
+            // Create the button panel for the check and reset buttons
             JPanel buttonPanel = new JPanel();
             buttonPanel.setLayout(new GridLayout(1, 2, 10, 10));
             buttonPanel.add(checkButton);
             buttonPanel.add(resetButton);
 
+            // Add the input panel, result scroll pane, and button panel to the main panel
             mainPanel.add(inputPanel, BorderLayout.NORTH);
             mainPanel.add(resultScrollPane, BorderLayout.CENTER);
             mainPanel.add(buttonPanel, BorderLayout.SOUTH);
 
+            // Add the main panel to the main frame and display
             frame.add(mainPanel);
             frame.setVisible(true);
         });
     }
 
-    // check the validity
+    // Check if a card number is valid for a given type of card
     public static boolean isValid(long n, String type) {
         if (type.equals("VISA") && (Long.toString(n).length() == 13 || Long.toString(n).length() == 16)) {
             return true;
@@ -103,19 +114,16 @@ public class CreditGUI {
         }
     }
 
-    // get what type of card
+    // Determine the type of a card based on its number
     public static String getType(long n) {
-        // visa
+        // Check if the card number is a Visa, Amex, or Mastercard
+        // using the first few digits of the number and Luhn's algorithm
         if (Long.toString(n).charAt(0) == '4' && checksum(n)) {
             return "VISA";
-        }
-        // amex
-        else if ((Long.toString(n).substring(0, 2).equals("34") || Long.toString(n).substring(0, 2).equals("37"))
+        } else if ((Long.toString(n).substring(0, 2).equals("34") || Long.toString(n).substring(0, 2).equals("37"))
                 && checksum(n)) {
             return "AMEX";
-        }
-        // mastercard
-        else if (Long.parseLong(Long.toString(n).substring(0, 2)) >= 51
+        } else if (Long.parseLong(Long.toString(n).substring(0, 2)) >= 51
                 && Long.parseLong(Long.toString(n).substring(0, 2)) <= 55 && checksum(n)) {
             return "MASTERCARD";
         } else {
@@ -123,10 +131,9 @@ public class CreditGUI {
         }
     }
 
-    // Luhn's algorithm
+    // Calculate the checksum using Luhn's algorithm
     public static boolean checksum(long n) {
-        // multiply every other digit by 2, starting with the number's second-to-last
-        // digit,
+        // Multiply every other digit by 2, starting with the number's second-to-last digit,
         // and then add those products' digits together.
         int sum = 0;
         for (int i = Long.toString(n).length() - 2; i >= 0; i -= 2) {
@@ -135,12 +142,11 @@ public class CreditGUI {
             sum += product % 10;
             sum += product / 10;
         }
-        // add the sum to the sum of the digits that weren't multiplied by 2.
+        // Add the sum to the sum of the digits that weren't multiplied by 2.
         for (int i = Long.toString(n).length() - 1; i >= 0; i -= 2) {
             sum += Integer.parseInt(Character.toString(Long.toString(n).charAt(i)));
         }
-        // if the total's last digit is 0 (or, put more formally, if the total modulo 10
-        // is congruent to 0),
+        // If the total's last digit is 0 (or, put more formally, if the total modulo 10 is congruent to 0),
         // the number is valid!
         if (sum % 10 == 0) {
             return true;
@@ -149,3 +155,4 @@ public class CreditGUI {
         }
     }
 }
+
